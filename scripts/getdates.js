e9b47a2d1854ec1cbaa108719ff39990033d7b12
katyounge <katyounge@hotmail.com>
2023-09-28 12:24:25 -0700

    site plan

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,12 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = `Last Modification: ${new Date(document.lastModified)}`;
+
+
+const mainnav = document.querySelector('.navigation');
+const hamButton = document.querySelector('#menu');
+
+hamButton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hamButton.classList.toggle('show');
+});
+	
