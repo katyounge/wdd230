19fdab84b8b56b282395a89954e7cefc221f61cb
katyounge <katyounge@hotmail.com>
2023-10-14 16:11:39 -0700

    dark mode mess

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -5,3 +5,36 @@ hamButton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hamButton.classList.toggle('show');
 });
+
+const toggleSwitch = document.querySelector('#switch');
+const main = document.querySelector("main");
+
+toggleSwitch.addEventListener('click', () => {
+	// toggleSwitch.classList.toggle('active');
+	console.log(toggleSwitch)
+	toggleSwitch.classList.toggle('on');
+	if (toggleSwitch.input.value == "true") {
+		toggleSwitch.input.value = "false";
+		main.style.background = "#000";
+		main.style.color = "#fff";
+	}
+	else {
+		toggleSwitch.input.value = "true";
+		main.style.background = "#fff";
+		main.style.color = "#000";
+	}
+});
+
+
+
+// toggleSwitch.addEventListener('click', () => {
+// 	if(toggleSwitch.classList.toggle('on')) {
+// 		main.style.background = "#000";
+// 		main.style.color = "#fff";
+// 	}
+// })
+
+    // toggleSwitch.addEventListener('click', () => {
+    //   toggle.classList.toggle('on');
+	//   document.getElementsByTagName('main').style.color = "white";
+    // });
