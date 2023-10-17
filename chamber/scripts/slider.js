806ccf3ec34d117d086aabe5970c8f781e798d12
katyounge <katyounge@hotmail.com>
2023-10-17 12:56:21 -0700

    slider added

---
+++ chamber/scripts/slider.js
@@ -0,0 +1,19 @@
+var img = document.getElementById('img1');
+
+var slides = ['images/cheris-place.jpg','images/duvall-auto.jpg','images/foundry-bar-and-grill.png'];
+
+var Start=0;
+
+function slider(){
+    if(Start<slides.length){
+        Start=Start+1;
+    }
+    else{
+        Start=1;
+    }
+    console.log(img);
+    img.innerHTML = "<img src="+slides[Start-1]+">";
+   
+}
+
+setInterval(slider,4000);
