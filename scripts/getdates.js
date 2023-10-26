a363b18313fb6e3a7261e6207ad26478a865accf
katyounge <katyounge@hotmail.com>
2023-10-26 08:43:20 -0700

    page counter added

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,5 @@
+// Last Updated
+
 document.getElementById("year").innerHTML = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = `Last Modification: ${new Date(document.lastModified)}`;
 
@@ -10,3 +12,17 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('show');
 });
 	
+// Page Visitor Count
+
+const visitsDisplay = document.querySelector('.visits');
+let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem('numVisits-1s', numVisits);
