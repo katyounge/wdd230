ada2e3a73e50dd5c6dea991aa85f235acac7d247
katyounge <katyounge@hotmail.com>
2023-10-31 12:30:48 -0700

    added a form to the homepage

---
+++ scripts/form.js
@@ -0,0 +1,32 @@
+const pass1 = document.querySelector('#password');
+const pass2 = document.querySelector('#password2');
+
+const msg = document.querySelector('#formmsg');
+
+pass2.addEventListener('focusout', checkSame);
+
+function checkSame() {
+    if (pass1.value == pass2.value) {
+        msg.style.display = 'none';
+        pass2.style.backgroundColor = 'white';
+        pass2.style.color = '#000';
+    } else {
+        msg.textContent = '❗Passwords DO NOT MATCH';
+        msg.style.visibility = 'show';
+        pass2.style.backgroundColor = '#fff0f3';
+        pass2.value = '';
+        pass2.focus();
+
+    }
+}
+
+
+const rangevalue = document.getElementById('rangevalue');
+const range = document.getElementById('range');
+
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
