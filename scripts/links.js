30dc8eade10f7409228c6653b16c07a7a37fab1d
katyounge <katyounge@hotmail.com>
2023-11-07 10:51:05 -0800

    work on new JS

--- scripts/links.js
+++ scripts/links.js
@@ -5,8 +5,36 @@ const linksURL = "https://katyounge.github.io/wdd230/data/links.json";
 async function getLinkData(url) {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.lessons);
-    // displayProphets(data.prophets);
+    // console.table(data.lessons);
+    displayLinks(data);
 }
 
 getLinkData(linksURL);
+
+const displayLinks = (weeks) => {
+    // linkList.innerHTML = '';
+    let linkList = document.querySelector('.lesson-links');
+    
+}
+
+weeks.forEach((week) => {
+    
+    let lessonItem = document.createElement('li');
+    
+
+    lessonItem.setAttribute("class", "linkItem");
+    lessonItem.textContent = `Week ${week.lesson}: `
+
+    
+    weeks.links.forEach((activity) => {
+        let linkURL = document.createElement('a');
+        linkURL.settAttribute("href", activity.url);
+        linkURL.textContent = `${activity.title} | `;
+        lessonItem.textContent.append = linkURL;
+    });
+
+    
+    linkList.appendChild(lessonItem);
+
+});
+
