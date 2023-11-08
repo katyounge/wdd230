185c15604b6a8552d2126bb73415bac67a4920d0
katyounge <katyounge@hotmail.com>
2023-11-07 18:01:19 -0800

    formatting and it works now!

--- scripts/links.js
+++ scripts/links.js
@@ -20,21 +20,25 @@ const displayLinks = (weeks) => {
     
     weeks.forEach((week) => {
 
-        
+        let isFirst = true;
         let lessonItem = document.createElement('li');
-        let linkData = `<a href="${week.links.url}" title="${week.links.title}">${week.links.title}</a>`
-
         lessonItem.setAttribute("class", "linkItem");
-        lessonItem.innerHTML = `Week ${week.lesson}: ${linkData}`;
+        lessonItem.innerHTML = `Week ${week.lesson}: `;
 
-        // weeks.links.forEach((activity) => {
-        //     let linkURL = document.createElement('a');
-        //     linkURL.settAttribute("href", activity.url);
-        //     linkURL.textContent = `${activity.title} | `;
-        //     lessonItem.textContent.append(linkURL);
+        week.links.forEach((activity) => {
+            let url = activity.url;
+            let name = activity.title;
+
+            if (isFirst === true) {
+                isFirst = false;
+
+            } else {
+                lessonItem.innerHTML += ` |`;
+            }
+
+            lessonItem.innerHTML += ` <a href="${url}" title="${name}">${name}</a>`;
             
-        // });
-
+        });
 
         linkList.appendChild(lessonItem);
 
@@ -44,27 +48,3 @@ const displayLinks = (weeks) => {
 }
 
 
-const displayProphets = (prophets) => {
-    prophets.forEach((prophet) => {
-        let card = document.createElement('section');
-        let fullName = document.createElement('h2');
-        let portrait = document.createElement('img');
-        let dob = document.createElement('p');
-        let pob = document.createElement('p');
-        card.setAttribute("class", "prophet-card");
-        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
-        dob.textContent =  `Date of Birth: ${prophet.birthdate}`;
-        pob.textContent =  `Place of Birth: ${prophet.birthplace}`;
-        portrait.setAttribute("src", prophet.imageurl);
-        portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
-        portrait.setAttribute("loading", "lazy");
-        portrait.setAttribute("width", '340');
-        portrait.setAttribute("height", '440');
-        card.appendChild(fullName);
-        card.appendChild(dob);
-        card.appendChild(pob);
-        card.appendChild(portrait);
-        cards.appendChild(card);
-
-    });
-}
